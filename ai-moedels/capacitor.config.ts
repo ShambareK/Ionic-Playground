32e84b857156edd5cf93cf3de15bb582653c9a81
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2023-11-15 22:16:37 +0000

    Iitialized App

---
+++ ai-moedels/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ai-moedels',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
